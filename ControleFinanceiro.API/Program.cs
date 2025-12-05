ef4922c6169b9e6f5cf979bab974c5b82caf3b99
Miguel Kotowey <miguel.kotowey@hotmail.com>
2025-12-05 00:01:30 -0300

    Configurações CSPROJ 2

--- ControleFinanceiro.API/Program.cs
+++ ControleFinanceiro.API/Program.cs
@@ -1,11 +1,20 @@
+using ControleFinanceiro.Core.Repositories;
+using ControleFinanceiro.Data;
+using ControleFinanceiro.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 var app = builder.Build();
 
@@ -17,9 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
