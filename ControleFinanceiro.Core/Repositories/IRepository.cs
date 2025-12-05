1b344f978f6aa2772aa4980fed2c82a283b8d1b9
Miguel Kotowey <miguel.kotowey@hotmail.com>
2025-12-05 00:02:12 -0300

    Tratamento e preparo da entidade TiposDespesa
    - Controller
    - DTO
    - Repository (para tratamento de métodos gerais)
    - AppDbContext

---
+++ ControleFinanceiro.Core/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Core.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        // Leitura
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetManyAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+
+        // Escrita
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+
+        // Aux
+        Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ ControleFinanceiro.Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ControleFinanceiro.Core.Entidades;
+using ControleFinanceiro.Data.Configuracoes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TipoDespesa> TiposDespesa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new TiposDespesaConfiguration());
+        }
+    }
+}
